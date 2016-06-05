ed6db142ff6d262a637ab92e088ebd771a8713e3
zaffarhaider <zaffar_haider@hotmail.com>
2016-06-05 22:20:10 +0200

    The updated program can read the given coloumns from booking.csv and print the first five entries.

--- exercise2.py
+++ exercise2.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 #Exercise-2: top 10 arrival airports in the world in 2013 (using the bookings file)
 
-fields = ['arr_port', 'pax'] # select the required fields
-
-df = pd.read_csv('bookings.csv',sep=',', usecols=fields) # read the data from the selected coloumns
+def arrivals():
+    fields = ['arr_port','pax'] # select the required fields
+    bookings = pd.read_csv('bookings.csv',sep='^',usecols=fields) # read the data from the selected coloumns
+    print('\n First 5 records')
+    print bookings.head()

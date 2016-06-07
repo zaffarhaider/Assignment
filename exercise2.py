cfe06f64a9ddad8b2427dfeff321331742d4082a
zaffarhaider <zaffar_haider@hotmail.com>
2016-06-07 16:22:47 +0200

    The program prints to 10 arrival airports

--- exercise2.py
+++ exercise2.py
@@ -1,10 +1,10 @@
-import csv
 import pandas as pd
 
 #Exercise-2: top 10 arrival airports in the world in 2013 (using the bookings file)
-
-def arrivals():
+def arr_arrivals():
     fields = ['arr_port','pax'] # select the required fields
     bookings = pd.read_csv('bookings.csv',sep='^',usecols=fields) # read the data from the selected coloumns
-    print('\n First 5 records')
-    print bookings.head()
+    #group cloumn: arr_ports and count value in 'pax', then sort values to get maximum number of arrivals
+    result= bookings.groupby('arr_port')['pax'].count().sort_values(ascending=False) 
+    print result[:10] #print to 10 recodrs of dataframe
+    
